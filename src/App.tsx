fc69a176d062ea1822f4b05bc49acae41f35de91
Aritz <aritz_egiguren@hotmail.com>
2021-10-07 19:11:31 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/form';
 
 function App() {
+  const [contador, setContador] = useState(0);
+  // console.log(contador);
+  // const setMiContador = (event: number) => {
+  //   setContador(prev => prev + 1)
+  // };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Form />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>{contador}</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <button onClick={() => setMiContador(1)}>
+          Enter
+        </button> */}
       </header>
     </div>
   );
